ee2400d3d9e6423c84c298c517bb19ea6fcbd959
fcg.1400019345 <tnnygles@hotmail.com>
2015-06-11 11:46:26 -0400

    tarefa 2 completa

--- app/src/main/java/com/example/fcg1400019345/horoscopo/AtividadePrincipal.java
+++ app/src/main/java/com/example/fcg1400019345/horoscopo/AtividadePrincipal.java
@@ -4,15 +4,53 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @SuppressWarnings("deprecation")
 public class AtividadePrincipal extends ActionBarActivity {
+    private ListAdapter mAdaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal);
+
+        String [] dados= {
+                "Áres - 21/03 a 20/04",
+                "Touro - 21/04 a 20/05",
+                "Gêmeos - 21/05 a 20/06",
+                "Câncer - 21/06 a 21/07",
+                "Leão - 22/07 a 22/08",
+                "Virgem - 23/08 a 22/09",
+                "Libra - 23/09 a 22/10",
+                "Escorpião - 23/10 a 21/11",
+                "Sargitário - 22/11 a 21/12",
+                "Capricórnio - 22/12 a 20/01",
+                "Aquário - 21/01 a 19/02",
+                "Peixes - 20/02 a 20/03"
+
+
+        };
+
+        List<String> signos = new ArrayList<>(Arrays.asList(dados));
+
+        //CRIAR O ADAPTADOR
+        mAdaptador= new ArrayAdapter<>(
+                getApplicationContext(), //contexto atual
+                R.layout.item_lista_principal, //nome do ID do layout
+                R.id.item_texto, //ID do TextView a ser prenchido
+                signos);
+
+        ListView listView = (ListView) findViewById(R.id.lista_principal);
+        listView.setAdapter(mAdaptador);
+
     }
 
     @Override
